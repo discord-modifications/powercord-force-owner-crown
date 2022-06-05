e060ed9693f529e7f2bb1844b3270cf9b47d2130
eternal <eternal.git@gmail.com>
2022-06-05 15:25:24 +0100

    fix

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const { Plugin } = require('powercord/entities');
-const { getModule, getModuleByDisplayName } = require('powercord/webpack');
+const { getModule } = require('powercord/webpack');
 const { inject, uninject } = require('powercord/injector');
 
 const { getGuild } = getModule(['getGuild'], false);
-const MemberListItem = getModuleByDisplayName('MemberListItem', false);
+const Item = getModule(['AVATAR_DECORATION_PADDING'], false)?.default;
 
 module.exports = class ForceOwnerCrown extends Plugin {
    startPlugin() {
-      inject('force-owner-crown', MemberListItem.prototype, 'renderDecorators', function (args, res) {
-         const owner = getGuild(this.props.channel?.guild_id)?.ownerId;
-         if (owner == this.props.user?.id) this.props.isOwner = true;
+      if (!Item) return;
 
-         return args;
-      }, true);
+      inject('force-owner-crown-wrapper', Item, 'type', function (_, res) {
+         inject('force-owner-crown', res.type.prototype, 'renderDecorators', function (args, res) {
+            const owner = getGuild(this.props.channel?.guild_id)?.ownerId;
+            if (owner == this.props.user?.id) this.props.isOwner = true;
+
+            return args;
+         }, true);
+
+         uninject('force-owner-crown-wrapper');
+         return res;
+      });
    }
 
    pluginWillUnload() {
+      uninject('force-owner-crown-wrapper');
       uninject('force-owner-crown');
    }
 };
